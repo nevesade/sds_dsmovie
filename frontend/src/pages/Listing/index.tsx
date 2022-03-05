433fb0f12b5ae0c60fe7bf3fea224aee2cd07327
nevesade <adenasiocarvalho19@gmail.com>
2022-03-05 11:04:16 +0000

    MovieCard fixed reposivity card grid

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,12 +7,37 @@ function Listing() {
 
         <>
             <Pagination />
-            <MovieCard />
-         
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+
+
+                </div>
+            </div>
+
+
 
         </>
-       );
-       
+    );
+
 }
 
 export default Listing;
